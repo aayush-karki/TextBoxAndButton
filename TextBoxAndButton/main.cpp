2d4ae0ccd462eb3e7f5b511734857249a5f2b9bb
Aayush Karki <akarki@ramapo.edu>
2022-03-04 16:10:00 -0500

    textBox class made

--- TextBoxAndButton/main.cpp
+++ TextBoxAndButton/main.cpp
@@ -1,4 +1,12 @@
 #include "SFML/Graphics.hpp"
+#include <filesystem>
+
+#include <iostream>
+#include <sstream>
+
+#include "TextBox.h"
+
+// ==========================================================================
 
 int main()
 {
@@ -7,17 +15,37 @@ int main()
 
 	// event handler
 	sf::Event events;
-	
 	// shape
 	sf::CircleShape circle;
-	circle.setFillColor( sf::Color::Red );
-	circle.setRadius( 100 );
+	//circle.setFillColor( sf::Color::Red );
+
+	// getting the cwd
+	std::string cwd = std::filesystem::current_path().string();
+	sf::Font font;
+	if( !font.loadFromFile( cwd + "\\Fonts\\arial.ttf" ) )
+	{
+		std::cout << "Error!!! while loading font" << std::endl;
+	}
+
+	TextBox myTextBox( 25, sf::Color::White, false);
+	myTextBox.SetFont( font );
+	myTextBox.SetPosition( sf::Vector2f( 200, 200 ) );
+	myTextBox.SetLimit( true, 0 );
 
 	bool close = false;
 
 	// app main loop
 	while( !close )
 	{
+		if( sf::Keyboard::isKeyPressed( sf::Keyboard::Return ) )
+		{
+			myTextBox.setSelected( true );
+		}
+		else if( sf::Keyboard::isKeyPressed( sf::Keyboard::Escape ) )
+		{
+			myTextBox.setSelected( false );
+		}
+
 		// handeling events
 		while( window.pollEvent( events ) )
 		{
@@ -25,13 +53,17 @@ int main()
 			{
 				close = true;
 			}
+			else if( events.type == sf::Event::TextEntered )
+			{
+				myTextBox.TypeOn( events );
+			}
 		}
 
 		// logics
 
 		// rendering
 		window.clear();
-		window.draw( circle );
+		myTextBox.Draw( window );
 		window.display();
 
 	}
